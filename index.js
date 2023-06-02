fa4de70aaf46f089eb1f0b7ec1548631e8f26b82
Amber-shah <92028798+Amber-shah@users.noreply.github.com>
2023-06-02 15:38:41 +0530

    1 file modified

--- index.js
+++ index.js
@@ -31,3 +31,46 @@ var obj1={
   }
 }
 obj1.myArrow()
+
+//Spread Operator
+//denoted as ...
+// 4 types of SO :- Expanding string, array and Combinig array, Object
+
+//EXPANDING STRING
+let str = 'hello';
+let arrStr = [...str];
+console.log(str);
+console.log(arrStr);
+
+//Expanding ARRAY
+let exp = ["hello","I","Am","learning","js"]
+console.log(exp)
+console.log(...exp)
+
+//Combining Array
+let arr1 = ["hello","I","Am","learning","js"]
+let arr2 = ["for", "lwc"]
+let arr3 = [...arr1,...arr2]
+console.log(arr3)
+
+//Combining object
+var obj = {"name":"Amber",age:23}
+var obj2 = {"name":"Shah",age:21}
+
+var obj3 = {...obj,...obj2}
+console.log(obj3)         //Overrides same attribute
+
+obj2= {"name":"Shah",age:21,"gender":"male"}
+var obj3 = {...obj,...obj2}
+console.log(obj3)
+
+obj2= {"Position":"SDE",package:800000,"gender":"male"}
+var obj3 = {...obj,...obj2}
+console.log(obj3)
+
+
+
+
+
+
+
