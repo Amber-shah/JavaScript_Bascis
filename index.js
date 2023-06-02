a7046fdceb4a802218eb4d55e562d76da7ad7a3c
Amber-shah <92028798+Amber-shah@users.noreply.github.com>
2023-06-02 12:36:42 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ console.log(a, b, ...arr);
 console.log(a, b,...rest);
 console.log(arr)
 
-//
+//Normal Function
+function abc(){
+  console.log("hello")
+}
+abc()
+
+// arrow function
+xyz = ()=>{
+  console.log('hello arrow')
+}
+xyz()
+
+// arrow with no parameter and paranthesis
+// applicable for single output and input
+
+xyz = _=> console.log('hello arrow')
+xyz()
